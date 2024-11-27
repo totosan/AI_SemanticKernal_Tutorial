df4c50221644a17c85581b9f02212e6563115b51
Thomas Tomow <toto_san@live.com>
2024-11-27 18:39:04 +0100

    first version

---
+++ semantickernelsample/NativePlugIns/LightPlugin.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semantickernelsample.NativePlugIns
+{
+    internal class LightPlugin
+    {
+        public bool IsOn { get; set; } = false;
+
+//#pragma warning disable CA1024 // Use properties where appropriate
+        [KernelFunction]
+        [Description("Gets the state of the light.")]
+        public string GetState() => IsOn ? "on" : "off";
+//#pragma warning restore CA1024 // Use properties where appropriate
+
+        [KernelFunction]
+        [Description("Changes the state of the light.'")]
+        public string ChangeState(bool newState)
+        {
+            this.IsOn = newState;
+            var state = GetState();
+
+            PaintBox(newState);
+
+            // Print the state to the console
+            Console.WriteLine($"[Light is now {state}]");
+
+            return state;
+        }
+
+            [KernelFunction]
+        [Description("Invoked for any intent to repair the car. For the given service and the cpdm product k-type specified by user, it locates the product information inside Herth-Bush CPDM system and looksup the product information.")]
+        public Task<string> LookupProduct(
+              [Description("The name of the service, for which the user is interested.")]  string serviceName,
+              [Description("The name of the product inside CPDM. Also called k-type")] string productName,
+              [Description("The user's ask or intent")] string intent)
+        {
+            return Task.FromResult<string>("VW Bremse. https://cpdmurl.com/bremse/77");
+        }
+
+
+
+        protected static void PaintBox(bool onoff)
+        {
+            // Define box dimensions
+            int boxWidth = 2;
+            int boxHeight = 2;
+
+            // Define the color of the box
+            ConsoleColor boxColor = onoff ? ConsoleColor.Green : ConsoleColor.Gray;
+
+            // Get the dimensions of the console window
+            int consoleWidth = Console.WindowWidth;
+            int consoleHeight = Console.WindowHeight;
+
+            // Calculate the top-right position for the box
+            int startX = consoleWidth - boxWidth;
+            int startY = 0;
+
+            var pos = Console.GetCursorPosition();
+
+            // Draw the box
+            Console.ForegroundColor = boxColor;
+
+            for (int y = startY; y < startY + boxHeight; y++)
+            {
+                Console.SetCursorPosition(startX, y);
+                Console.Write(new string('█', boxWidth)); // Use █ for a solid block
+            }
+
+            // Reset console color
+            Console.ResetColor();
+
+            Console.SetCursorPosition(pos.Left, pos.Top);
+
+        }
+    }
+}
---
+++ semantickernelsample/NativePlugIns/SamplePlugIn.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.SemanticKernel;
+//using Microsoft.SemanticKernel.Orchestration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semantickernelsample.Skills
+{
+    public class SamplePlugIn
+    {
+        private readonly Kernel? _kernel;
+
+        public SamplePlugIn(Kernel? kernel = null)
+        {
+            _kernel = kernel;
+        }
+
+        [KernelFunction, Description("Adds additional information to the model. Performs grounding.")]
+        public string Enrich()
+        {
+            return "Mention that November 28 is the day the great DotNet Developer Conference is hold in Cologne with a great base of people learning about AI.";
+        }
+
+
+        //[KernelFunction, Description("State machine.")]
+        //public Task<SKContext> AddValue()
+        //{
+        //    Debug.WriteLine(context.GetHashCode());
+
+        //    if (!context.Variables.ContainsKey("mystate"))
+        //        context.Variables.Set("mystate", "0");
+
+        //    var state = context.Variables["mystate"];
+
+        //    context.Variables.Set("mystate", "stopped!");
+
+        //    return Task.FromResult<SKContext>(context);
+        //}
+
+
+        //[KernelFunction, Description("State machine.")]
+        //public string GetValue()
+        //{
+        //    if (!context.Variables.ContainsKey("mystate"))
+        //        context.Variables.Set("mystate", "0");
+
+        //    var state = context.Variables["mystate"];
+
+        //    return state;
+        //}
+
+
+        [KernelFunction, Description("Does nothing. Demonstrates how to access the context.")]
+        public string NullFunction()
+        {
+            return null;
+        }
+
+        [KernelFunction, Description("Adds two numbers.")]
+        public int AddNumbersFunction(int arg1, int arg2)
+        {
+            ExecutionContext context = ExecutionContext.Capture();
+
+            return (arg1 + arg2);
+        }
+
+
+        [KernelFunction, Description("Executes the function semantically extracted from prompt.")]
+        public async Task<string> ExecuteMathOperationFunction(string prompt)
+        {
+            var plugIn = this._kernel?.ImportPluginFromPromptDirectory("SamplePlugin");
+
+            var res = await _kernel.InvokeAsync<string>(plugIn["MathOperationExtractor"], new() { ["input"] = prompt });
+
+            var tokens = res.Split('|');
+
+            var mathOperator = tokens[0];
+
+            var args = tokens[1].Split(',');
+
+            var arg1 = int.Parse(args[0].Replace("\nArguments: ", String.Empty));
+            var arg2 = int.Parse(args[1]);
+
+            switch (mathOperator.Replace("Function: ", String.Empty))
+            {
+                case "+":
+                    return (arg1 + arg2).ToString();
+
+                case "exponent":
+                    return (Math.Pow(arg1, arg2)).ToString();
+                default:
+                    return "unknown operator";
+            }
+        }
+
+        [KernelFunction, Description("Calculates the fiction function.")]
+        public Task<string> FictionFunction(
+            [Description("The first argument that describes some entity")] string input,
+            [Description("The second argument that describes some entity")] string arg2,
+            [Description("The the number that defines the contraction jumping of sausages units between entities used to calculate the fiction.")] int number)
+        {
+            return Task.FromResult<string>($"{input}-{arg2}-{number.ToString()}");
+        }
+
+
+        [KernelFunction, Description("Berechnet Investment in die Technologie im Kontext eines Portfolios.")]
+        public Task<string> WorkshopFunction(
+            [Description("Investment Bereich, Sektor usw.")] string input,
+            [Description("The second argument that describes the name of the customer")] string customer,
+            [Description("Zeitraum")] TimeSpan timeRange)
+        {
+            return Task.FromResult<string>($"{input}-{timeRange}-{customer}");
+        }
+
+        [KernelFunction, Description("Books working hours in Employe service. User wants to commit wotking hours.")]
+        public Task<string> EmployeeServiceBookHoursFunction(
+        [Description("The first argument that specifies the project name for which the working hours will be booked.")] string project,
+        [Description("Number of working hours to be booked on the project")] string workingHours,
+        [Description("Number of working hours to be booked on the project")] string date)
+        {
+            return Task.FromResult<string>($"{project}-{workingHours}-{date}");
+        }
+
+
+
+      
+    }
+}
